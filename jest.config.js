3c6336522cd1aa93a17089809a39dd5fde88554b
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-11-07 12:59:07 -0500

    UPDATE

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '**/*.(t|j)s',
+    '!**/*.spec.ts',
+    '!**/*.e2e-spec.ts',
+    '!**/dto/**',
+    '!**/*.entity.ts',
+    '!main.ts'
+  ],
+  coverageDirectory: '../coverage',
+  coverageProvider: 'v8',
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: './coverage',
+      outputName: 'junit.xml',
+      classNameTemplate: '{classname}',
+      titleTemplate: '{title}',
+      ancestorSeparator: ' › ',
+      usePathForSuiteName: true
+    }]
+  ],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/$1'
+  }
+};
